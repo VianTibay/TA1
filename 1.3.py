0957d86bce824d2df6c25b7cc025b229eee0eb33
Vian <202310591@fit.edu.ph>
2025-02-08 04:38:52 +0800

    refactor(change): added a comment description

--- 1.3.py
+++ 1.3.py
@@ -1,4 +1,4 @@
-
+#  A program that will display the following given output. (for a. use nested for statement and for b. use nested while statement)
 rows = 5
 for i in range(1, rows + 1):
     print(' ' * (rows - i) + ''.join(str(j) for j in range(1, i + 1)))
